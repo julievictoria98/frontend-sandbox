12135ca7518eb57dc811bdf8e08191f7a64489a2
julievictoria98 <jura0002@stud.kea.dk>
2024-10-28 16:33:35 +0100

    tjekker

---
+++ src/components/Button.jsx
@@ -0,0 +1,12 @@
+import { FaCookieBite } from "react-icons/fa";
+
+const Button = () => {
+    return ( 
+        <div>
+            <button>Click me</button>
+            <FaCookieBite />
+        </div>
+     );
+}
+ 
+export default Button;
