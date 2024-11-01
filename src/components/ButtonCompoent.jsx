2e4112abfb3f43fc2de9ca0832bd44963ebf279b
julievictoria98 <jura0002@stud.kea.dk>
2024-11-01 09:01:00 +0100

    idk

---
+++ src/components/ButtonCompoent.jsx
@@ -0,0 +1,9 @@
+const CookieContentCTA = ({ ctatext }) => {
+  return (
+    <button className=" w-full dark:bg-black bg-white text-black dark:text-white p-2  rounded-lg">
+      {ctatext}
+    </button>
+  );
+};
+
+export default CookieContentCTA;
