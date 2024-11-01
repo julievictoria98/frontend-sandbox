e8794884e968e281ebf1f3bb84010ba6e723caef
julievictoria98 <jura0002@stud.kea.dk>
2024-11-01 10:22:58 +0100

    lavet onClick state

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,20 +1,37 @@
+"use client";
 import { FaCookieBite } from "react-icons/fa";
 import { IoIosClose } from "react-icons/io";
+import { useState } from "react";
 
 const Button = () => {
+  const [isCookieShown, setisCookieShown] = useState(true);
   return (
-    <div className="max-w-64 flex flex-col items-start gap-8 bg-black rounded-xl p-6">
-      <div className="flex justify-between w-full mb-4">
-        <FaCookieBite color="white" size={50} />
-        <IoIosClose color="white" size={30} className="cursor-pointer" />
+    isCookieShown && (
+      <div className="max-w-64 flex flex-col items-start gap-8 bg-black rounded-xl p-6">
+        <div className="flex justify-between w-full mb-4">
+          <FaCookieBite color="white" size={50} />
+          <IoIosClose
+            onClick={() => {
+              setisCookieShown(!isCookieShown);
+            }}
+            color="white"
+            size={30}
+            className="cursor-pointer"
+          />
+        </div>
+        <p className="max-w-40 text-white">
+          We use cookies to improve your user experince
+        </p>
+        <button
+          onClick={() => {
+            setisCookieShown(!isCookieShown);
+          }}
+          className=" w-full bg-white text-black p-2  rounded-lg"
+        >
+          I like cookies
+        </button>
       </div>
-      <p className="max-w-40 text-white">
-        We use cookies to improve your user experince
-      </p>
-      <button className=" w-full bg-white text-black p-2  rounded-lg">
-        I like cookies
-      </button>
-    </div>
+    )
   );
 };
 
--- src/components/ButtonCompoent.jsx
+++ src/components/ButtonCompoent.jsx
@@ -1,6 +1,8 @@
+import { useState } from "react";
 const CookieContentCTA = ({ ctatext }) => {
+  const [cookieButton, setCookieButton] = useState(false);
   return (
-    <button className=" w-full dark:bg-black bg-white text-black dark:text-white p-2  rounded-lg">
+    <button className=" w-full dark:bg-black bg-white text-black dark:text-white p-2  rounded-lg cursor-pointer">
       {ctatext}
     </button>
   );
