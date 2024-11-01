c549dff0bb70b006caec9d0b2308ed4819008e72
julievictoria98 <jura0002@stud.kea.dk>
2024-11-01 08:36:12 +0100

    rettet i react-icons

--- src/components/CookieContent.jsx
+++ src/components/CookieContent.jsx
@@ -4,12 +4,15 @@ import CookieContentCTA from "./CookieContentCTA";
 
 const CookieContent = () => {
   return (
-    <div className="max-w-64 flex flex-col items-start gap-8 bg-black dark:bg-white rounded-xl p-6">
+    <div className="max-w-64 flex flex-col items-start gap-8 dark:bg-white bg-black  rounded-xl p-6">
       <div className="flex justify-between w-full mb-4">
-        <FaCookieBite color="white" size={50} />
-        <IoIosClose color="white" size={30} className="cursor-pointer" />
+        <FaCookieBite className="text-white dark:text-black" size={50} />
+        <IoIosClose
+          size={30}
+          className=" text-white dark:text-black cursor-pointer"
+        />
       </div>
-      <p className="max-w-40 text-white">
+      <p className="max-w-40 text-white dark:text-black">
         We use cookies to improve your user experince
       </p>
       <CookieContentCTA ctatext="I like Cookies" />
--- src/components/CookieContentCTA.jsx
+++ src/components/CookieContentCTA.jsx
@@ -1,6 +1,6 @@
 const CookieContentCTA = ({ ctatext }) => {
   return (
-    <button className=" w-full bg-white text-black dark:text-white p-2  rounded-lg">
+    <button className=" w-full dark:bg-black bg-white text-black dark:text-white p-2  rounded-lg">
       {ctatext}
     </button>
   );
